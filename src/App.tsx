c72309327a157ab5a162f105fdbbe56690cc0f44
giovanaassis <giovana_2011_11@hotmail.com>
2025-04-12 16:32:14 -0300

    feat: initial config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="app font-dmsans">
+        <h1 className="text-3xl underline text-(--color-primary-500) ">Fit Track</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
