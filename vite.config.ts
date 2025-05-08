e3b89511f5e6c0b80bde75cfd0de3f2d4f6a6acc
giovanaassis <giovana_2011_11@hotmail.com>
2025-05-07 22:37:39 -0300

    chore: config vitest and testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -6,7 +7,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest-setup.ts"
+  },
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
-  }
+  },
 })
---
+++ vitest-setup.ts
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { vitest } from "vitest";
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vitest.fn(), // deprecated
+    removeListener: vitest.fn(), // deprecated
+    addEventListener: vitest.fn(),
+    removeEventListener: vitest.fn(),
+    dispatchEvent: vitest.fn(),
+  }),
+});
