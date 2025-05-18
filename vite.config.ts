e291d16d77f01a8dc95a341c5a9eb688a679afd0
giovanaassis <giovana_2011_11@hotmail.com>
2025-05-18 01:51:34 -0300

    chore: update vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./vitest-setup.ts"
+    setupFiles: "./vitest-setup.tsx"
   },
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
